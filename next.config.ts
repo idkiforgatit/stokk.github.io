5fe10066138f6b81a326c1a46715c6b7e33071ec
Stokk Developer <stokk-dev@example.com>
2026-05-08 11:30:42 +0300

    Fix: Enable backend by moving index.html to stokk.html

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/",
+        destination: "/stokk.html",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
